47d1a9731413572f9dc6c049d6c06cdd62c6d42b
Adam Szabo <adam.szabo@global.com>
2026-09-11 18:00:28 +0100

    Update minimum target version to iOS 18.0

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Nimble",
     platforms: [
-      .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)
+      .macOS(.v10_15), .iOS("18.0"), .tvOS("18.0"), .watchOS("11.0"), .visionOS(.v1)
     ],
     products: [
         .library(
